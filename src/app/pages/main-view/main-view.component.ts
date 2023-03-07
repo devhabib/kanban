622dc71226c1b933782ad8219ab2652e28d1ecd3
devhabib <habibr152@gmail.com>
2023-03-07 15:27:20 +0600

    version 1 updated

---
+++ src/app/pages/main-view/main-view.component.ts
@@ -0,0 +1,51 @@
+import { Board } from './../../models/board.model';
+import { Column } from './../../models/column.model';
+import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-main-view',
+  templateUrl: './main-view.component.html',
+  styleUrls: ['./main-view.component.scss']
+})
+export class MainViewComponent {
+
+  board: Board = new Board('Test Board', [
+    new Column('Ideas', [
+      'Some Random Idea',
+      'This is another random Idea',
+      'Get well soon :/',
+    ]),
+    new Column('Research', [
+      'Random Research',
+      'Random Research 2',
+      'Random Research  3',
+      'Random Research  4'
+    ]),
+    new Column('Todo', [
+      'Get to work',
+      'Pick up groceries',
+      'Go home',
+      'Fall asleep'
+    ]),
+    new Column('Done', [
+      'Get up',
+      'Brush teeth',
+      'Take a shower',
+      'Check e-mail',
+      'Walk dog'
+    ]),
+
+  ]);
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
+}
